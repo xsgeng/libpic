a0698fa5e2c02df50833230b8959235f4784c1dd
gengxs <xsgeng@siom.ac.cn>
2023-07-19 13:41:12 +0800

    add axis for fields

--- libpic/fields.py
+++ libpic/fields.py
@@ -1,8 +1,8 @@
 import numpy as np
-from maxwell_2d import update_bfield_2d, update_efield_2d
+from .maxwell_2d import update_bfield_2d, update_efield_2d
 
 class Fields2D:
-    def __init__(self, nx, ny, dx, dy, n_guard) -> None:
+    def __init__(self, nx, ny, dx, dy, x0, y0, n_guard) -> None:
         self.nx = nx
         self.ny = ny
         self.dx = dx
@@ -19,6 +19,16 @@ class Fields2D:
         self.jx = np.zeros(shape)
         self.jy = np.zeros(shape)
         self.jz = np.zeros(shape)
+
+        xaxis = np.arange(nx+n_guard*2, dtype=float)
+        xaxis[-n_guard:] = np.arange(-n_guard, 0)
+        xaxis *= dx
+        self.xaxis = xaxis[:, None] + x0
+
+        yaxis = np.arange(ny+2*n_guard, dtype=float)
+        yaxis[-n_guard:] = np.arange(-n_guard, 0)
+        yaxis *= dy
+        self.yaxis = yaxis[None, :] + y0
 
     def __getitem__(self, key):
         return self.ex[key], self.ey[key], self.ez[key], self.bx[key], self.by[key], self.bz[key], self.jx[key], self.jy[key], self.jz[key]
