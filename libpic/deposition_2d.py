a2990194950c1fd4109bdace46ab328cfbca4e9b
gengxs <xsgeng@siom.ac.cn>
2023-08-01 16:27:17 +0800

    fix current deposition vectorization with nan in particle

--- libpic/deposition_2d.py
+++ libpic/deposition_2d.py
@@ -4,7 +4,7 @@ from scipy.constants import mu_0, epsilon_0, c, e
 
 nbuff = 64
 
-@njit(cache=True)
+@njit
 def current_deposit_2d(rho, jx, jy, jz, x, y, ux, uy, uz, inv_gamma, pruned, npart, dx, dy, x0, y0, dt, w, q):
     """
     Current deposition in 2D for CPU.
@@ -36,6 +36,13 @@ def current_deposit_2d(rho, jx, jy, jz, x, y, ux, uy, uz, inv_gamma, pruned, npa
     q : float
         Charge of the particles.
     """
+
+    x_old = np.zeros(nbuff) 
+    y_old = np.zeros(nbuff) 
+    x_adv = np.zeros(nbuff) 
+    y_adv = np.zeros(nbuff) 
+    vz = np.zeros(nbuff) 
+   
     S0x = np.zeros((5, nbuff))
     S1x = np.zeros((5, nbuff))
     S0y = np.zeros((5, nbuff))
@@ -48,85 +55,53 @@ def current_deposit_2d(rho, jx, jy, jz, x, y, ux, uy, uz, inv_gamma, pruned, npa
         npart_buff = min(nbuff, npart - ibuff)
         for ip in range(npart_buff):
             ipart_global = ibuff + ip
+            if pruned[ipart_global]:
+                vz[ip] = 0.0
+                x_old[ip] = 0.0
+                y_old[ip] = 0.0
+                x_adv[ip] = 0.0
+                y_adv[ip] = 0.0
+                continue
             vx = ux[ipart_global]*c*inv_gamma[ipart_global]
             vy = uy[ipart_global]*c*inv_gamma[ipart_global]
-            vz = uz[ipart_global]*c*inv_gamma[ipart_global]
-            x_old = x[ipart_global] - vx*0.5*dt - x0
-            y_old = y[ipart_global] - vy*0.5*dt - y0
-            x_adv = x[ipart_global] + vx*0.5*dt - x0
-            y_adv = y[ipart_global] + vy*0.5*dt - y0
+            vz[ip] = uz[ipart_global]*c*inv_gamma[ipart_global] if ~pruned[ipart_global] else 0.0
+            x_old[ip] = x[ipart_global] - vx*0.5*dt - x0
+            y_old[ip] = y[ipart_global] - vy*0.5*dt - y0
+            x_adv[ip] = x[ipart_global] + vx*0.5*dt - x0
+            y_adv[ip] = y[ipart_global] + vy*0.5*dt - y0
 
+        for ip in range(npart_buff):
+            ipart_global = ibuff + ip
             # positions at t + dt/2, before pusher
             # +0.5 for cell-centered coordinate
-            x_over_dx0 = x_old / dx
+            x_over_dx0 = x_old[ip] / dx
             ix0 = int(np.floor(x_over_dx0+0.5))
-            y_over_dy0 = y_old / dy
+            y_over_dy0 = y_old[ip] / dy
             iy0 = int(np.floor(y_over_dy0+0.5))
 
-            delta = x_over_dx0 - ix0
-            delta2 = delta * delta
-            S0x[0, ip] = 0.0
-            S0x[1, ip] = 0.5 * ( delta2+delta+0.25 )
-            S0x[2, ip] = 0.75 - delta2
-            S0x[3, ip] = 0.5 * ( delta2-delta+0.25 )
-            S0x[4, ip] = 0.0
-
-            delta = y_over_dy0 - iy0
-            delta2 = delta * delta
-            S0y[0, ip] = 0.0
-            S0y[1, ip] = 0.5 * ( delta2+delta+0.25 )
-            S0y[2, ip] = 0.75 - delta2
-            S0y[3, ip] = 0.5 * ( delta2-delta+0.25 )
-            S0y[4, ip] = 0.0
+            calculate_S(x_over_dx0 - ix0, 0, ip, S0x)
+            calculate_S(y_over_dy0 - iy0, 0, ip, S0y)
 
             # positions at t + 3/2*dt, after pusher
-            x_over_dx1 = x_adv / dx
+            x_over_dx1 = x_adv[ip] / dx
             ix1 = int(np.floor(x_over_dx1+0.5))
             dcell_x = ix1 - ix0
 
-            y_over_dy1 = y_adv / dy
+            y_over_dy1 = y_adv[ip] / dy
             iy1 = int(np.floor(y_over_dy1+0.5))
             dcell_y = iy1 - iy0
 
-            delta = x_over_dx0 - ix0
-            delta2 = delta * delta
-            delta_minus = 0.5 * ( delta2+delta+0.25 )
-            delta_mid = 0.75 - delta2
-            delta_positive = 0.5 * ( delta2-delta+0.25 )
-
-            minus = dcell_x == -1
-            mid = dcell_x == 0
-            positive = dcell_x == 1
-            S1x[0, ip] = minus * delta_minus
-            S1x[1, ip] = minus * delta_mid      + mid * delta_minus
-            S1x[2, ip] = minus * delta_positive + mid * delta_mid      + positive * delta_minus
-            S1x[3, ip] =                          mid * delta_positive + positive * delta_mid
-            S1x[4, ip] =                                                 positive * delta_positive
-
-            delta = y_over_dy0 - iy0
-            delta2 = delta * delta
-            delta_minus = 0.5 * ( delta2+delta+0.25 )
-            delta_mid = 0.75 - delta2
-            delta_positive = 0.5 * ( delta2-delta+0.25 )
-
-            minus = dcell_y == -1
-            mid = dcell_y == 0
-            positive = dcell_y == 1
-            S1y[0, ip] = minus * delta_minus
-            S1y[1, ip] = minus * delta_mid      + mid * delta_minus
-            S1y[2, ip] = minus * delta_positive + mid * delta_mid      + positive * delta_minus
-            S1y[3, ip] =                          mid * delta_positive + positive * delta_mid
-            S1y[4, ip] =                                                 positive * delta_positive
+            calculate_S(x_over_dx1 - ix1, dcell_x, ip, S1x)
+            calculate_S(y_over_dy1 - iy1, dcell_y, ip, S1y)
 
             for i in range(5):
                 DSx[i, ip] = S1x[i, ip] - S0x[i, ip]
                 DSy[i, ip] = S1y[i, ip] - S0y[i, ip]
                 jy_buff[i, ip] = 0
 
-        for ip in range(npart_buff):
             one_third = 1.0 / 3.0
-            charge_density = q * w[ibuff+ip] / (dx*dy) 
-            charge_density *= ~pruned[ibuff+ip]
+            charge_density = q * w[ipart_global] / (dx*dy) 
+            charge_density *= ~pruned[ipart_global]
             factor = charge_density / dt
             
 
@@ -140,6 +115,7 @@ def current_deposit_2d(rho, jx, jy, jz, x, y, ux, uy, uz, inv_gamma, pruned, npa
                 iy = iy0 + (j - 2)
                 for i in range(min(1, 1+dcell_x), max(4, 4+dcell_x)):
                     ix = ix0 + (i - 2)
+
                     wx = DSx[i, ip] * (S0y[j, ip] + 0.5 * DSy[j, ip])
                     wy = DSy[j, ip] * (S0x[i, ip] + 0.5 * DSx[i, ip])
                     wz = S0x[i, ip] * S0y[j, ip] + 0.5 * DSx[i, ip] * S0y[j, ip] \
@@ -150,8 +126,27 @@ def current_deposit_2d(rho, jx, jy, jz, x, y, ux, uy, uz, inv_gamma, pruned, npa
 
                     jx[ix, iy] += jx_buff
                     jy[ix, iy] += jy_buff[i, ip]
-                    jz[ix, iy] += factor * wz * vz
+                    jz[ix, iy] += factor * wz * vz[ip]
                     rho[ix, iy] += charge_density * S1x[i, ip] * S1y[j, ip]
+
+
+@njit(inline="always")
+def calculate_S(delta, shift, ip, S):
+    delta2 = delta * delta
+
+    delta_minus    = 0.5 * ( delta2+delta+0.25 )
+    delta_mid      = 0.75 - delta2
+    delta_positive = 0.5 * ( delta2-delta+0.25 )
+
+    minus = shift == -1
+    mid = shift == 0
+    positive = shift == 1
+
+    S[0, ip] = minus * delta_minus
+    S[1, ip] = minus * delta_mid      + mid * delta_minus
+    S[2, ip] = minus * delta_positive + mid * delta_mid      + positive * delta_minus
+    S[3, ip] =                          mid * delta_positive + positive * delta_mid
+    S[4, ip] =                                                 positive * delta_positive
 
 
 def test_current():
