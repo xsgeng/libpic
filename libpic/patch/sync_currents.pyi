dc825b205499a6751c99a3b276f8c7b9d8f4715f
gengxs <xsgeng@siom.ac.cn>
2025-02-25 13:29:00 +0800

    feat: Add sync_currents implementation and type hints

---
+++ libpic/patch/sync_currents.pyi
@@ -0,0 +1,13 @@
+from typing import List
+import numpy as np
+from ...fields import Fields
+
+def sync_currents(
+    fields_list: List[Fields],
+    xmin_index_list: np.ndarray,
+    xmax_index_list: np.ndarray,
+    ymin_index_list: np.ndarray,
+    ymax_index_list: np.ndarray,
+    npatches: int, nx: int, ny: int, ng: int
+):
+    pass
