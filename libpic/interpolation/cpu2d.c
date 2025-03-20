b10a94450b7d6257e5a50291d1ce7ec4298f06fa
gengxs <xsgeng@siom.ac.cn>
2025-03-20 14:25:42 +0800

    refactor: Simplify 2D interpolation code and remove redundant functions

--- libpic/interpolation/cpu2d.c
+++ libpic/interpolation/cpu2d.c
@@ -2,13 +2,7 @@
 #include <numpy/arrayobject.h>
 #include <omp.h>
 #include <math.h>
-
-#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-#define INDEX(i, j, nx, ny) \
-    ((j) >= 0 ? (j) : (j) + (ny)) + \
-    ((i) >= 0 ? (i) : (i) + (nx)) * (ny)
-
-
+#include "../utils/cutils.h"
 
 inline static void get_gx(double delta, double* gx) {
     double delta2 = delta * delta;
@@ -21,252 +15,119 @@ inline static void get_gx(double delta, double* gx) {
  * adapted from https://github.com/Warwick-Plasma/epoch/blob/main/epoch2d/src/include/triangle/e_part.inc
  * and https://github.com/Warwick-Plasma/epoch/blob/main/epoch2d/src/include/triangle/b_part.inc
  */
-inline static double interp_ex(double* ex, double* hx, double* gy, npy_intp ix2, npy_intp iy1, npy_intp nx, npy_intp ny) {
-    double ex_part = 
-          gy[0] * (hx[0] * ex[INDEX(ix2-1, iy1-1, nx, ny)] 
-        +          hx[1] * ex[INDEX(ix2,   iy1-1, nx, ny)] 
-        +          hx[2] * ex[INDEX(ix2+1, iy1-1, nx, ny)])
-        + gy[1] * (hx[0] * ex[INDEX(ix2-1, iy1,   nx, ny)] 
-        +          hx[1] * ex[INDEX(ix2,   iy1,   nx, ny)] 
-        +          hx[2] * ex[INDEX(ix2+1, iy1,   nx, ny)]) 
-        + gy[2] * (hx[0] * ex[INDEX(ix2-1, iy1+1, nx, ny)] 
-        +          hx[1] * ex[INDEX(ix2,   iy1+1, nx, ny)] 
-        +          hx[2] * ex[INDEX(ix2+1, iy1+1, nx, ny)]);
-    return ex_part;
+inline static double interp_field(double* field, double* fac1, double* fac2, npy_intp ix, npy_intp iy, npy_intp nx, npy_intp ny) {
+    double field_part = 
+          fac2[0] * (fac1[0] * field[INDEX2(ix-1, iy-1)] 
+        +            fac1[1] * field[INDEX2(ix,   iy-1)] 
+        +            fac1[2] * field[INDEX2(ix+1, iy-1)])
+        + fac2[1] * (fac1[0] * field[INDEX2(ix-1, iy  )] 
+        +            fac1[1] * field[INDEX2(ix,   iy  )] 
+        +            fac1[2] * field[INDEX2(ix+1, iy  )]) 
+        + fac2[2] * (fac1[0] * field[INDEX2(ix-1, iy+1)] 
+        +            fac1[1] * field[INDEX2(ix,   iy+1)] 
+        +            fac1[2] * field[INDEX2(ix+1, iy+1)]);
+    return field_part;
 }
 
-inline static double interp_ey(double* ey, double* gx, double* hy, npy_intp ix1, npy_intp iy2, npy_intp nx, npy_intp ny) {
-    double ey_part =
-          hy[ 0] * (gx[ 0] * ey[INDEX(ix1-1,iy2-1, nx, ny)]
-        +           gx[ 1] * ey[INDEX(ix1  ,iy2-1, nx, ny)]
-        +           gx[ 2] * ey[INDEX(ix1+1,iy2-1, nx, ny)])
-        + hy[ 1] * (gx[ 0] * ey[INDEX(ix1-1,iy2  , nx, ny)]
-        +           gx[ 1] * ey[INDEX(ix1  ,iy2  , nx, ny)]
-        +           gx[ 2] * ey[INDEX(ix1+1,iy2  , nx, ny)])
-        + hy[ 2] * (gx[ 0] * ey[INDEX(ix1-1,iy2+1, nx, ny)]
-        +           gx[ 1] * ey[INDEX(ix1  ,iy2+1, nx, ny)]
-        +           gx[ 2] * ey[INDEX(ix1+1,iy2+1, nx, ny)]);
-    return ey_part;
-}
-
-inline static double interp_ez(double* ez, double* gx, double* gy, npy_intp ix1, npy_intp iy1, npy_intp nx, npy_intp ny) {
-    double ez_part =
-          gy[ 0] * (gx[ 0] * ez[INDEX(ix1-1, iy1-1, nx, ny)]
-        +           gx[ 1] * ez[INDEX(ix1  , iy1-1, nx, ny)]
-        +           gx[ 2] * ez[INDEX(ix1+1, iy1-1, nx, ny)])
-        + gy[ 1] * (gx[ 0] * ez[INDEX(ix1-1, iy1  , nx, ny)]
-        +           gx[ 1] * ez[INDEX(ix1  , iy1  , nx, ny)]
-        +           gx[ 2] * ez[INDEX(ix1+1, iy1  , nx, ny)])
-        + gy[ 2] * (gx[ 0] * ez[INDEX(ix1-1, iy1+1, nx, ny)]
-        +           gx[ 1] * ez[INDEX(ix1  , iy1+1, nx, ny)]
-        +           gx[ 2] * ez[INDEX(ix1+1, iy1+1, nx, ny)]);
-    return ez_part;
-}
-
-inline static double interp_bx(double* bx, double* gx, double* hy, npy_intp ix1, npy_intp iy2, npy_intp nx, npy_intp ny) {
-    double bx_part =
-          hy[ 0] * (gx[ 0] * bx[INDEX(ix1-1, iy2-1, nx, ny)]
-        +           gx[ 1] * bx[INDEX(ix1  , iy2-1, nx, ny)]
-        +           gx[ 2] * bx[INDEX(ix1+1, iy2-1, nx, ny)])
-        + hy[ 1] * (gx[ 0] * bx[INDEX(ix1-1, iy2  , nx, ny)]
-        +           gx[ 1] * bx[INDEX(ix1  , iy2  , nx, ny)]
-        +           gx[ 2] * bx[INDEX(ix1+1, iy2  , nx, ny)])
-        + hy[ 2] * (gx[ 0] * bx[INDEX(ix1-1, iy2+1, nx, ny)]
-        +           gx[ 1] * bx[INDEX(ix1  , iy2+1, nx, ny)]
-        +           gx[ 2] * bx[INDEX(ix1+1, iy2+1, nx, ny)]);
-    return bx_part;
-}
-
-inline static double interp_by(double* by, double* hx, double* gy, npy_intp ix2, npy_intp iy1, npy_intp nx, npy_intp ny) {
-    double by_part = 
-          by_part =
-          gy[ 0] * (hx[ 0] * by[INDEX(ix2-1, iy1-1, nx, ny)]
-        +           hx[ 1] * by[INDEX(ix2  , iy1-1, nx, ny)]
-        +           hx[ 2] * by[INDEX(ix2+1, iy1-1, nx, ny)])
-        + gy[ 1] * (hx[ 0] * by[INDEX(ix2-1, iy1  , nx, ny)]
-        +           hx[ 1] * by[INDEX(ix2  , iy1  , nx, ny)]
-        +           hx[ 2] * by[INDEX(ix2+1, iy1  , nx, ny)])
-        + gy[ 2] * (hx[ 0] * by[INDEX(ix2-1, iy1+1, nx, ny)]
-        +           hx[ 1] * by[INDEX(ix2  , iy1+1, nx, ny)]
-        +           hx[ 2] * by[INDEX(ix2+1, iy1+1, nx, ny)]);
-    return by_part;
-}
-
-inline static double interp_bz(double* bz, double* hx, double* hy, npy_intp ix2, npy_intp iy2, npy_intp nx, npy_intp ny) {
-    double bz_part =
-          hy[ 0] * (hx[ 0] * bz[INDEX(ix2-1, iy2-1, nx, ny)]
-        +           hx[ 1] * bz[INDEX(ix2  , iy2-1, nx, ny)]
-        +           hx[ 2] * bz[INDEX(ix2+1, iy2-1, nx, ny)])
-        + hy[ 1] * (hx[ 0] * bz[INDEX(ix2-1, iy2  , nx, ny)]
-        +           hx[ 1] * bz[INDEX(ix2  , iy2  , nx, ny)]
-        +           hx[ 2] * bz[INDEX(ix2+1, iy2  , nx, ny)])
-        + hy[ 2] * (hx[ 0] * bz[INDEX(ix2-1, iy2+1, nx, ny)]
-        +           hx[ 1] * bz[INDEX(ix2  , iy2+1, nx, ny)]
-        +           hx[ 2] * bz[INDEX(ix2+1, iy2+1, nx, ny)]);
-    return bz_part;
-}
-
-static void interpolation_2d(
-    double* x, double* y, 
+inline static void interpolation_2d(
+    double x, double y, 
     double* ex_part, double* ey_part, double* ez_part, 
     double* bx_part, double* by_part, double* bz_part, 
-    npy_bool* is_dead,
-    npy_intp npart, 
     double* ex, double* ey, double* ez, 
     double* bx, double* by, double* bz, 
     double dx, double dy, double x0, double y0, 
-    npy_intp nx, npy_intp ny) {
+    npy_intp nx, npy_intp ny
+) {
     
     double gx[3];
     double gy[3];
     double hx[3];
     double hy[3];
+    
+    double x_over_dx = (x - x0) / dx;
+    double y_over_dy = (y - y0) / dy;
 
-    for (npy_intp ip = 0; ip < npart; ip++) {
-        if (is_dead[ip]) {
-            continue;
-        }
-        // print if x or y is nan
-        if (isnan(x[ip]) || isnan(y[ip])) {
-            printf("x or y is nan but not dead.\n");
-            continue;
-        }
-        
-        double x_over_dx = (x[ip] - x0) / dx;
-        double y_over_dy = (y[ip] - y0) / dy;
+    npy_intp ix1 = (int)floor(x_over_dx + 0.5);
+    get_gx(ix1 - x_over_dx, gx);
 
-        npy_intp ix1 = (int)floor(x_over_dx + 0.5);
-        get_gx(ix1 - x_over_dx, gx);
+    npy_intp ix2 = (int)floor(x_over_dx);
+    get_gx(ix2 - x_over_dx + 0.5, hx);
 
-        npy_intp ix2 = (int)floor(x_over_dx);
-        get_gx(ix2 - x_over_dx + 0.5, hx);
+    npy_intp iy1 = (int)floor(y_over_dy + 0.5);
+    get_gx(iy1 - y_over_dy, gy);
 
-        npy_intp iy1 = (int)floor(y_over_dy + 0.5);
-        get_gx(iy1 - y_over_dy, gy);
+    npy_intp iy2 = (int)floor(y_over_dy);
+    get_gx(iy2 - y_over_dy + 0.5, hy);
 
-        npy_intp iy2 = (int)floor(y_over_dy);
-        get_gx(iy2 - y_over_dy + 0.5, hy);
+    *ex_part = interp_field(ex, hx, gy, ix2, iy1, nx, ny);
+    *ey_part = interp_field(ey, gx, hy, ix1, iy2, nx, ny);
+    *ez_part = interp_field(ez, gx, gy, ix1, iy1, nx, ny);
 
-        ex_part[ip] = interp_ex(ex, hx, gy, ix2, iy1, nx, ny);
-        ey_part[ip] = interp_ey(ey, gx, hy, ix1, iy2, nx, ny);
-        ez_part[ip] = interp_ez(ez, gx, gy, ix1, iy1, nx, ny);
-
-        bx_part[ip] = interp_bx(bx, gx, hy, ix1, iy2, nx, ny);
-        by_part[ip] = interp_by(by, hx, gy, ix2, iy1, nx, ny);
-        bz_part[ip] = interp_bz(bz, hx, hy, ix2, iy2, nx, ny);
-    }
-}
-
-/**
- * Python wrapper for the interpolation_2d function.
- */
-PyObject* _interpolation_2d(PyObject* self, PyObject* args) {
-    PyArrayObject *x, *y, 
-                *ex_part, *ey_part, *ez_part, 
-                *bx_part, *by_part, *bz_part, 
-                *is_dead,
-                *ex, *ey, *ez, 
-                *bx, *by, *bz;
-    double dx, dy, x0, y0;
-    npy_intp npart, nx, ny;
-
-    if (!PyArg_ParseTuple(args, "OOOOOOOOOnOOOOOOddddnn", 
-        &x, &y, 
-        &ex_part, &ey_part, &ez_part, 
-        &bx_part, &by_part, &bz_part, 
-        &is_dead,
-        &npart, 
-        &ex, &ey, &ez, 
-        &bx, &by, &bz, 
-        &dx, &dy, &x0, &y0, 
-        &nx, &ny)) {
-        return NULL;
-    }
-
-    interpolation_2d(
-        (double*)PyArray_DATA(x), 
-        (double*)PyArray_DATA(y), 
-        (double*)PyArray_DATA(ex_part), 
-        (double*)PyArray_DATA(ey_part), 
-        (double*)PyArray_DATA(ez_part), 
-        (double*)PyArray_DATA(bx_part), 
-        (double*)PyArray_DATA(by_part), 
-        (double*)PyArray_DATA(bz_part), 
-        (npy_bool*)PyArray_DATA(is_dead), 
-        npart, 
-        (double*)PyArray_DATA(ex), 
-        (double*)PyArray_DATA(ey), 
-        (double*)PyArray_DATA(ez), 
-        (double*)PyArray_DATA(bx), 
-        (double*)PyArray_DATA(by), 
-        (double*)PyArray_DATA(bz), 
-        dx, dy, x0, y0, 
-        nx, ny);
-
-    Py_RETURN_NONE;
+    *bx_part = interp_field(bx, gx, hy, ix1, iy2, nx, ny);
+    *by_part = interp_field(by, hx, gy, ix2, iy1, nx, ny);
+    *bz_part = interp_field(bz, hx, hy, ix2, iy2, nx, ny);
 }
 
 static PyObject* interpolation_patches_2d(PyObject* self, PyObject* args) {
-    PyObject *x_list, *y_list, 
-             *ex_part_list, *ey_part_list, *ez_part_list, 
-             *bx_part_list, *by_part_list, *bz_part_list, 
-             *ex_list, *ey_list, *ez_list, 
-             *bx_list, *by_list, *bz_list, 
-             *x0_list, *y0_list, 
-             *is_dead_list;
-    double dx, dy;
-    npy_intp npatches, nx, ny;
-
-    if (!PyArg_ParseTuple(args, "OOOOOOOOOOOOOOOOOnddnn", 
-        &x_list, &y_list, 
-        &ex_part_list, &ey_part_list, &ez_part_list, 
-        &bx_part_list, &by_part_list, &bz_part_list, 
-        &is_dead_list, 
-        &ex_list, &ey_list, &ez_list, 
-        &bx_list, &by_list, &bz_list, 
-        &x0_list, &y0_list, 
-        &npatches, &dx, &dy, &nx, &ny)) {
+    PyObject *fields_list, *particles_list;
+    npy_intp npatches;
+    if (!PyArg_ParseTuple(args, "OOn", 
+        &particles_list, &fields_list, 
+        &npatches)) {
         return NULL;
     }
 
     if (npatches <= 0) {
         Py_RETURN_NONE;
     }
+    
+    npy_intp nx = PyLong_AsLong(PyObject_GetAttrString(PyList_GET_ITEM(fields_list, 0), "nx"));
+    npy_intp ny = PyLong_AsLong(PyObject_GetAttrString(PyList_GET_ITEM(fields_list, 0), "ny"));
+    npy_intp n_guard = PyLong_AsLong(PyObject_GetAttrString(PyList_GET_ITEM(fields_list, 0), "n_guard"));
+    nx += 2*n_guard;
+    ny += 2*n_guard;
+    double dx = PyFloat_AsDouble(PyObject_GetAttrString(PyList_GET_ITEM(fields_list, 0), "dx"));
+    double dy = PyFloat_AsDouble(PyObject_GetAttrString(PyList_GET_ITEM(fields_list, 0), "dy"));
+
+    // fields
+    AUTOFREE double **ex         = get_attr_array_double(fields_list, npatches, "ex");
+    AUTOFREE double **ey         = get_attr_array_double(fields_list, npatches, "ey");
+    AUTOFREE double **ez         = get_attr_array_double(fields_list, npatches, "ez");
+    AUTOFREE double **bx         = get_attr_array_double(fields_list, npatches, "bx");
+    AUTOFREE double **by         = get_attr_array_double(fields_list, npatches, "by");
+    AUTOFREE double **bz         = get_attr_array_double(fields_list, npatches, "bz");
+    AUTOFREE double *x0          = get_attr_double(fields_list, npatches, "x0");
+    AUTOFREE double *y0          = get_attr_double(fields_list, npatches, "y0");
+
+    // particles
+    AUTOFREE double **x          = get_attr_array_double(particles_list, npatches, "x");
+    AUTOFREE double **y          = get_attr_array_double(particles_list, npatches, "y");
+    AUTOFREE double **ex_part    = get_attr_array_double(particles_list, npatches, "ex_part");
+    AUTOFREE double **ey_part    = get_attr_array_double(particles_list, npatches, "ey_part");
+    AUTOFREE double **ez_part    = get_attr_array_double(particles_list, npatches, "ez_part");
+    AUTOFREE double **bx_part    = get_attr_array_double(particles_list, npatches, "bx_part");
+    AUTOFREE double **by_part    = get_attr_array_double(particles_list, npatches, "by_part");
+    AUTOFREE double **bz_part    = get_attr_array_double(particles_list, npatches, "bz_part");
+    AUTOFREE npy_bool **is_dead  = get_attr_array_bool(particles_list, npatches, "is_dead");
+    AUTOFREE npy_intp *npart     = get_attr_int(particles_list, npatches, "npart");
 
     // release GIL
     Py_BEGIN_ALLOW_THREADS
     #pragma omp parallel for
     for (npy_intp ipatch = 0; ipatch < npatches; ipatch++) {
-        double *x          = (double*) PyArray_DATA((PyArrayObject*)PyList_GetItem(x_list, ipatch));
-        double *y          = (double*) PyArray_DATA((PyArrayObject*)PyList_GetItem(y_list, ipatch));
-        double *ex_part    = (double*) PyArray_DATA((PyArrayObject*)PyList_GetItem(ex_part_list, ipatch));
-        double *ey_part    = (double*) PyArray_DATA((PyArrayObject*)PyList_GetItem(ey_part_list, ipatch));
-        double *ez_part    = (double*) PyArray_DATA((PyArrayObject*)PyList_GetItem(ez_part_list, ipatch));
-        double *bx_part    = (double*) PyArray_DATA((PyArrayObject*)PyList_GetItem(bx_part_list, ipatch));
-        double *by_part    = (double*) PyArray_DATA((PyArrayObject*)PyList_GetItem(by_part_list, ipatch));
-        double *bz_part    = (double*) PyArray_DATA((PyArrayObject*)PyList_GetItem(bz_part_list, ipatch));
-        double *ex         = (double*) PyArray_DATA((PyArrayObject*)PyList_GetItem(ex_list, ipatch));
-        double *ey         = (double*) PyArray_DATA((PyArrayObject*)PyList_GetItem(ey_list, ipatch));
-        double *ez         = (double*) PyArray_DATA((PyArrayObject*)PyList_GetItem(ez_list, ipatch));
-        double *bx         = (double*) PyArray_DATA((PyArrayObject*)PyList_GetItem(bx_list, ipatch));
-        double *by         = (double*) PyArray_DATA((PyArrayObject*)PyList_GetItem(by_list, ipatch));
-        double *bz         = (double*) PyArray_DATA((PyArrayObject*)PyList_GetItem(bz_list, ipatch));
-        double x0          = PyFloat_AsDouble(PyList_GetItem(x0_list, ipatch));
-        double y0          = PyFloat_AsDouble(PyList_GetItem(y0_list, ipatch));
-        npy_bool *is_dead  = (npy_bool*) PyArray_DATA((PyArrayObject*)PyList_GetItem(is_dead_list, ipatch));
-
-        npy_intp npart = PyArray_DIM((PyArrayObject*)PyList_GetItem(x_list, ipatch), 0);
-
-        interpolation_2d(
-            x, y, 
-            ex_part, ey_part, ez_part, 
-            bx_part, by_part, bz_part, 
-            is_dead,
-            npart, 
-            ex, ey, ez, 
-            bx, by, bz, 
-            dx, dy, x0, y0, 
-            nx, ny);
+        for (npy_intp ip = 0; ip < npart[ipatch]; ip++) {
+            if (is_dead[ipatch][ip]) continue;
+            if (isnan(x[ipatch][ip]) || isnan(y[ipatch][ip])) continue;
+            interpolation_2d(
+                x[ipatch][ip], y[ipatch][ip], 
+                &ex_part[ipatch][ip], &ey_part[ipatch][ip], &ez_part[ipatch][ip], 
+                &bx_part[ipatch][ip], &by_part[ipatch][ip], &bz_part[ipatch][ip], 
+                ex[ipatch], ey[ipatch], ez[ipatch], 
+                bx[ipatch], by[ipatch], bz[ipatch], 
+                dx, dy, x0[ipatch], y0[ipatch], 
+                nx, ny
+            );
+        }
     }
     // reacquire GIL
     Py_END_ALLOW_THREADS
@@ -275,7 +136,6 @@ static PyObject* interpolation_patches_2d(PyObject* self, PyObject* args) {
 }
 
 static PyMethodDef InterpolationMethods[] = {
-    {"_interpolation_2d", _interpolation_2d, METH_VARARGS, "2D interpolation on CPU"},
     {"interpolation_patches_2d", interpolation_patches_2d, METH_VARARGS, "2D interpolation on CPU"},
     {NULL, NULL, 0, NULL}
 };
--- libpic/interpolation/cpu2d.pyi
+++ libpic/interpolation/cpu2d.pyi
@@ -1,25 +1,9 @@
-from numpy.typing import NDArray
-from numpy import float64, bool_
+from libpic.particles import ParticlesBase
+from libpic.fields import Fields
+from typing import List
 
 def interpolation_patches_2d(
-    x_list: list[NDArray[float64]], y_list: list[NDArray[float64]],
-    ex_part_list: list[NDArray[float64]], ey_part_list: list[NDArray[float64]], ez_part_list: list[NDArray[float64]],
-    bx_part_list: list[NDArray[float64]], by_part_list: list[NDArray[float64]], bz_part_list: list[NDArray[float64]],
-    is_dead_list: list[NDArray[bool_]],
-    ex_list: list[NDArray[float64]], ey_list: list[NDArray[float64]], ez_list: list[NDArray[float64]],
-    bx_list: list[NDArray[float64]], by_list: list[NDArray[float64]], bz_list: list[NDArray[float64]],
-    x0_list: list[float], y0_list: list[float],
-    npatches: int,
-    dx: float, dy: float, nx: int, ny: int
-) -> None: ...
-
-def _interpolation_2d(
-    x: NDArray[float64], y: NDArray[float64], 
-    ex_part: NDArray[float64], ey_part: NDArray[float64], ez_part: NDArray[float64], 
-    bx_part: NDArray[float64], by_part: NDArray[float64], bz_part: NDArray[float64], 
-    is_dead: NDArray[bool_],
-    npart: int,
-    ex: NDArray[float64], ey: NDArray[float64], ez: NDArray[float64], bx: NDArray[float64], by: NDArray[float64], bz: NDArray[float64],
-    dx: float, dy: float, x0: float, y0: float, 
-    nx: int, ny: int,
+    particles_list: List[ParticlesBase], 
+    fields_list: List[Fields], 
+    npatches: int, 
 ) -> None: ...
