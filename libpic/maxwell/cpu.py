2d7149d2a4b2ab7d9e90e82c125fd11e4c535c93
gengxs <xsgeng@siom.ac.cn>
2025-03-28 12:25:10 +0800

    refactor: optimize loop order in field update functions

--- libpic/maxwell/cpu.py
+++ libpic/maxwell/cpu.py
@@ -13,8 +13,8 @@ def update_efield_2d(
 ):
     bfactor = dt * c**2
     jfactor = dt / epsilon_0
-    for j in range(ny):
-        for i in range(nx):
+    for i in range(nx):
+        for j in range(ny):
             ex[i, j] += bfactor * ( (bz[i, j] - bz[i, j-1]) / dy) - jfactor * jx[i, j]
             ey[i, j] += bfactor * (-(bz[i, j] - bz[i-1, j]) / dx) - jfactor * jy[i, j]
             ez[i, j] += bfactor * ( (by[i, j] - by[i-1, j]) / dx - (bx[i, j] - bx[i, j-1]) / dy) - jfactor * jz[i, j]
@@ -27,8 +27,8 @@ def update_bfield_2d(
     dx, dy, dt, 
     nx, ny, n_guard
 ):
-    for j in range(ny):
-        for i in range(nx):
+    for i in range(nx):
+        for j in range(ny):
             bx[i, j] -= dt * ( (ez[i, j+1] - ez[i, j]) / dy)
             by[i, j] -= dt * (-(ez[i+1, j] - ez[i, j]) / dx)
             bz[i, j] -= dt * ( (ey[i+1, j] - ey[i, j]) / dx - (ex[i, j+1] - ex[i, j]) / dy)
@@ -86,9 +86,9 @@ def update_efield_3d(
 ):
     bfactor = dt * c**2
     jfactor = dt / epsilon_0
-    for k in range(nz):
+    for i in range(nx):
         for j in range(ny):
-            for i in range(nx):
+            for k in range(nz):
                 ex[i, j, k] += bfactor * ((bz[i, j, k] - bz[i, j-1, k]) / dy - (by[i, j, k] - by[i, j, k-1]) / dz) - jfactor * jx[i, j ,k]
                 ey[i, j, k] += bfactor * ((bx[i, j, k] - bx[i, j, k-1]) / dz - (bz[i, j, k] - bz[i-1, j, k]) / dx) - jfactor * jy[i, j, k]
                 ez[i, j, k] += bfactor * ((by[i, j, k] - by[i-1, j, k]) / dx - (bx[i, j, k] - bx[i, j-1, k]) / dy) - jfactor * jz[i, j, k]
@@ -101,9 +101,9 @@ def update_bfield_3d(
     dx, dy, dz, dt, 
     nx, ny, nz, n_guard
 ):
-    for k in range(nz):
+    for i in range(nx):
         for j in range(ny):
-            for i in range(nx):
+            for k in range(nz):
                 bx[i, j, k] -= dt * ((ez[i, j+1, k] - ez[i, j, k]) / dy - (ey[i, j, k+1] - ey[i, j, k]) / dz)
                 by[i, j, k] -= dt * ((ex[i, j, k+1] - ex[i, j, k]) / dz - (ez[i+1, j, k] - ez[i, j, k]) / dx)
                 bz[i, j, k] -= dt * ((ey[i+1, j, k] - ey[i, j, k]) / dx - (ex[i, j+1, k] - ex[i, j, k]) / dy)
